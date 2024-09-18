88b1bbee4a30e0e82a522eab6ba0e5006a371c93
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-18 14:52:29 +0200

    Added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+
+  components:{
+
+  },
+    data(){
+        return{
+          
+        }
+    }
+}
+</script>
+
+<template>
+  <header class="d-flex align-items-center">
+    <img src="https://www.yugioh-card.com/en/wp-content/uploads/2020/09/TCG_logo_500x500.png" alt="">
+    <h1>Yu-Gi-Oh Api</h1>
+  </header>
+</template>
+
+<style lang="scss"  scoped>
+@use 'bootstrap/scss/bootstrap.scss' as *;
+img{
+  width: 100px;
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,23 +1,19 @@
 <script>
-import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 export default{
 
   components:{
-    AppHeader,
-    AppMain
+
   },
     data(){
         return{
-
+          
         }
     }
 }
 </script>
 
 <template>
-   <AppHeader/>
-   <AppMain/>
+   <h2>Main</h2>
 </template>
 
 <style lang="scss"  scoped>
